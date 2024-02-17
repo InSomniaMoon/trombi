0226442199a8cc147bab978fdc61ceeab9f302df
Aurélie Lemoine <aurelielemoine45@gmail.com>
2024-02-17 19:50:57 +0100

    page question

--- src/app/components/question-page/question-page.component.ts
+++ src/app/components/question-page/question-page.component.ts
@@ -1,17 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Student } from '../../core/types/student.type';
+import { Question } from '../../core/types/question.type';
+import { Observable } from 'rxjs';
+import { WebSocketService } from '../../core/services/web-socket.service';
+import { AsyncPipe, JsonPipe, NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-question-page',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, NgIf, JsonPipe, NgClass],
   templateUrl: './question-page.component.html',
   styleUrl: './question-page.component.scss'
 })
-export class QuestionPageComponent {
+export class QuestionPageComponent implements OnInit {
 
-  @Input({ required: true })
-  students!: Student[];
+  question!: Observable<Question>;
+  answerSelected!: Number;
 
-  
+  constructor(private webService: WebSocketService) { }
+
+  ngOnInit(): void {
+    this.question = this.webService.question();
+  }
+
+  removeQuestion() {
+    this.webService.send("/closeQuestion", {});
+  }
+
+  saveQuestion() {
+    //capture
+    this.webService.send("/closeQuestion", {});
+  }
+
+  chooseAnswer(index: number) {
+    this.answerSelected = index;
+  }
+
 }
